79187dd174f7a168e2794bc64214704098fd8fb7
RyzhovIvanV <114861242+RyzhovIvanV@users.noreply.github.com>
2025-12-21 18:21:18 +0400

    Update like.js

--- scripts/like.js
+++ scripts/like.js
@@ -1,24 +1,17 @@
-/* этот скрипт использует такие имена классов:
-✦ like-icon — для svg-иконки анимированного сердца
-✦ card__like-button — для кнопки Like рядом с иконкой
-✦ card__icon-button — для кнопки, оборачивающей иконку
-✦ card__icon-button — для кнопки, оборачивающей иконку
-✦ is-liked — для обозначения состояния лайкнутой иконки в виде сердца
-✦ button__text — для обозначения текстового элемента внутри кнопки
-Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
-*/
-
 const likeHeartArray = document.querySelectorAll('.like-icon');
 const likeButtonArray = document.querySelectorAll('.card__like-button');
 const iconButtonArray = document.querySelectorAll('.card__icon-button');
 
 iconButtonArray.forEach((iconButton, index) => {
-  iconButton.onclick = () =>
+  iconButton.addEventListener('click', () => {
     toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
+  });
 });
 
 likeButtonArray.forEach((button, index) => {
-  button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
+  button.addEventListener('click', () => {
+    toggleIsLiked(likeHeartArray[index], button);
+  });
 });
 
 function toggleIsLiked(heart, button) {
@@ -27,15 +20,26 @@ function toggleIsLiked(heart, button) {
 }
 
 function setButtonText(heart, button) {
-  if ([...heart.classList].includes('is-liked')) {
-    setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Unlike'),
-      500
-    );
+  if (heart.classList.contains('is-liked')) {
+    setTimeout(() => {
+      button.querySelector('.button__text').textContent = 'Unlike';
+    }, 500);
   } else {
-    setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Like'),
-      500
-    );
+    setTimeout(() => {
+      button.querySelector('.button__text').textContent = 'Like';
+    }, 500);
   }
 }
+
+const saveButton = document.querySelector('.button-remember');
+const dialog = document.querySelector('#dialog-id');
+const dialogClose = dialog.querySelector('.button-save');
+
+saveButton.addEventListener('click', (e) => {
+  e.preventDefault(); 
+  dialog.showModal();
+});
+
+dialogClose.addEventListener('click', () => {
+  dialog.close();
+});
